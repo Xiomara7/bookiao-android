77469821f6f3aec7239e0b294d6b8543b9450b4f
Christian <christian.etpr10@gmail.com>
2014-11-03 15:24:52 -0400

    Added login functionality

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,19 @@
 angular.module('starter.controllers', ['ionic'])
 
-.controller('LoginCtrl', function($scope) {
+.controller('LoginCtrl', function($scope, $http, $location) {
+  // Function that handles login
+  $scope.login = function() {
+    $http.post('http://bookiao-api.herokuapp.com/api-token-auth/', {"email": $scope.email, "password": $scope.password}).
+      success(function(data, status, headers, config) {
+        if (status == 200) {
+          window.localStorage['token'] = data.token;
+          $location.path("/tab/dash");
+        };
+      }).
+      error(function(data, status, headers, config) {
+        alert("Error during login.");
+      });
+  }
 })
 
 .controller('TabCtrl', function($scope, $ionicModal) {
