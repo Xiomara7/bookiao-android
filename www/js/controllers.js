31e8a11bddd901eadf9af13ee9fc1d7d6fe26a25
Christian <christian.etpr10@gmail.com>
2014-11-04 18:36:30 -0400

    Register works with Business

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -65,13 +65,55 @@ angular.module('starter.controllers', ['ionic'])
   };
 })
 
-.controller('RegisterCtrl', function($scope, $location) {
-  $scope.login = function() {
+.controller('RegisterCtrl', function($scope, $location, $http) {
+  $scope.back = function() {
     $location.path("/login");
+  };
+
+  $scope.login = function() {
+    $http.post('http://bookiao-api.herokuapp.com/api-token-auth/', {"email": $scope.user.email, "password": $scope.user.password}).
+      success(function(data, status, headers, config) {
+        if (status == 200) {
+          window.localStorage['token'] = data.token;
+          $scope.createObject();
+        };
+      }).
+      error(function(data, status, headers, config) {
+        alert("Error during login. Please contact Christian.");
+      });
+  };
+
+  $scope.createUser = function() {
+    $scope.user = JSON.parse(window.localStorage['user']);
+    $http.post('http://bookiao-api.herokuapp.com/register/', $scope.user).
+      success($scope.login).
+      error(function(data, status, headers, config) {
+        alert('Error creating user. Please contact Christian.');
+      });
   }
+
+  $scope.createObject = function() {
+    $http.post($scope.user.objectUrl, $scope.user, {headers: {'Authorization': 'JWT ' + window.localStorage['token']}}).
+      success(function(data, status, headers, config) {
+        $location.path("/tab/dash");
+      }).
+      error(function(data, status, headers, config) {
+        alert('Error creating object. Please contact Christian.');
+      });
+  }
+
 })
 
 .controller('RegisterBusinessCtrl', function($scope, $location) {
+
+  $scope.user = { 'objectUrl': 'http://bookiao-api.herokuapp.com/businesses/'};
+
+  $scope.register = function() {
+    delete window.localStorage['user'];
+    window.localStorage['user'] = JSON.stringify($scope.user);
+
+    $scope.createUser();
+  }
 
 })
 
